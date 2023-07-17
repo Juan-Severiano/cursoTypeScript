f0c115a2dbfc40ce03bbb64f4c3c84a0e115bfd9
juan <juansoussev@gmail.com>
2023-07-17 18:26:37 -0300

    Tip Any, não é normalmente usado

---
+++ src/A0002-tipo-any/index.ts
@@ -0,0 +1,8 @@
+// Any representa qualquer tipo, normalmente não é colocado no código pq vc quer
+
+function showMsg(msg: string) {
+  return msg;
+}
+
+const msg = showMsg('asdasdasdasdasdasdasd');
+console.log(msg);
