31a5d74219c3c6c867c3c40136970d2a19d9943e
juan <juansoussev@gmail.com>
2023-07-17 18:47:31 -0300

    Tipo Object, normalmente e até melhorn utilizado é deixar o tipo por inferência

---
+++ src/A0004-tipo-object/index.ts
@@ -0,0 +1,8 @@
+// Não é recomendado colocar o type logo na declaração(ex. objeto: object) deixe que o TS pegue a inferência
+
+const objectA = {
+  keyA: 'val a',
+  keyB: 'val b',
+};
+
+console.log(objectA);
