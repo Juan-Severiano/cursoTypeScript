dda6316c992d390629d396e3479d810e3ece659c
juan <juansoussev@gmail.com>
2023-07-17 21:04:07 -0300

    Tipo enum, para qunado vc quiser ordenar as coisas a serem esdcolhidas

---
+++ src/A0009-tipo-enum/index.ts
@@ -0,0 +1,9 @@
+// Tipo Enum, semelhante ao Banco de Dados, o ENum no TS deixa que sejam escolhidas apenas as coisas que estão entra as opções
+
+enum Cores {
+  BLUE,
+  RED,
+  YELLOW,
+}
+
+console.log(Cores);
