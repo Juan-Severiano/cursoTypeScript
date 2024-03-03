15c123581e6ec6cfa482893011d4153112f38635
Francisco Juan <juansoussev@gmail.com>
2024-03-03 10:33:21 -0300

    [type any]

--- src/A0002-tipo-any/index.ts
+++ src/A0002-tipo-any/index.ts
@@ -1,8 +1,14 @@
-// Any representa qualquer tipo, normalmente não é colocado no código pq vc quer
+// // Any representa qualquer tipo, normalmente não é colocado no código pq vc quer
 
-function showMsg(msg: string) {
-  return msg;
+// function showMsg(msg: string) {
+//   return msg;
+// }
+
+// const msg = showMsg('asdasdasdasdasdasdasd');
+// console.log(msg);
+import prompt from 'prompt-sync';
+const userResponse = prompt();
+
+if (typeof userResponse === 'number') {
+  console.log('number');
 }
-
-const msg = showMsg('asdasdasdasdasdasdasd');
-console.log(msg);
